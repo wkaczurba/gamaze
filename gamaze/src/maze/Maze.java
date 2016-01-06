c39fcde5de8755e640e5e2a561aa9eac8d710f05
Witold Kaczurba <witold.kaczurba@gmail.com>
2016-01-06 22:38:52 +0000

    unused text deleted.

--- gamaze/src/maze/Maze.java
+++ gamaze/src/maze/Maze.java
@@ -2,34 +2,6 @@ package maze;
 
 import java.util.Iterator;
 
-/*class MazeIterator implements Iterator<Integer> {
-	int i,j;
-	Maze m;
-	
-	MazeIterator(Maze m) {
-		this.i = 0;
-		this.j = 0;
-		this.m = m;
-	}
-	
-	@Override
-	public boolean hasNext() {
-		// FIXME: Check for size (maze can be of size 0)
-		
-		if (i != m.width - 1 && j != m.height - 1)
-			return true;
-
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Integer next() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-}*/
-
 // Make generic + abstract maze, then do intMaze. (?)
 public class Maze extends Object /*implements Iterable<Integer>*/ {
 	// array + some functions.
